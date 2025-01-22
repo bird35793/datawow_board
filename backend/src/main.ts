a685bdaa56f8281b423121132b1824e30f2dd929
bird35793.dev <bird35793.dev@gmail.com>
2025-01-22 23:47:20 +0700

    fix: config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv'; // import dotenv
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  dotenv.config(); // โหลดตัวแปรจาก .env
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe()); // เพิ่ม ValidationPipe
+  const port = process.env.PORT || 3000; // กำหนด port โดยอ่านจาก .env หรือใช้ 3000 เป็นค่า default
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`); // แสดง port ที่ใช้ใน console
 }
 bootstrap();
